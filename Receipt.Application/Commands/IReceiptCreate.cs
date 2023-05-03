611b32fca06e90ce21c098015aa47da69a71b32c
Rasmus Krog Sørensen <rkso38918@edu.ucl.dk>
2023-05-03 09:42:27 +0200

    Receipt done

---
+++ Receipt.Application/Commands/IReceiptCreate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receipt.Application.Commands
+{
+    public interface IReceiptCreate
+    {
+        public void ReceiptCreate(ReceiptCreateDto dto, string guid); 
+    }
+}
---
+++ Receipt.Application/Commands/Implementation/ReceiptCreate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Receipt._Domain.Model;
+using Receipt.Application.Repository;
+
+namespace Receipt.Application.Commands.Implementation
+{
+    public class ReceiptCreate: IReceiptCreate
+    {
+        private readonly IReceiptRepository _repository;
+
+        public ReceiptCreate(IReceiptRepository repository)
+        {
+            _repository = repository;
+        }
+
+        void IReceiptCreate.ReceiptCreate(ReceiptCreateDto dto, string guid)
+        {
+            var model = new ReceiptEntity(guid, dto.Screws, dto.Bolts, dto.Nails, dto.Price, dto.Cvr);
+            _repository.CreateReceipt(model);
+        }
+    }
+}
